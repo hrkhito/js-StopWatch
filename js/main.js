eddabf53fc9e4adfb8ff3026fc9a996b11674f82
haruki yamaguchi <hrk1106hasu@gmail.com>
2022-09-18 19:04:45 +0900

    finish

--- js/main.js
+++ js/main.js
@@ -23,38 +23,49 @@
   }
 
   const setButtonStateInitial=()=>{
-    start.disabled=false;
-    stop.disabled=true;
-    reset.disabled=true;
+    start.classList.remove('inactive')
+    stop.classList.add('inactive')
+    reset.classList.add('inactive')
   };
 
   const setButtonStateRunning=()=>{
-    start.disabled=true;
-    stop.disabled=false;
-    reset.disabled=true;
+    start.classList.add('inactive')
+    stop.classList.remove('inactive')
+    reset.classList.add('inactive')
   };
 
   const setButtonStateStopped=()=>{
-    start.disabled=false;
-    stop.disabled=true;
-    reset.disabled=false;
+    start.classList.remove('inactive')
+    stop.classList.add('inactive')
+    reset.classList.remove('inactive')
   };
 
   setButtonStateInitial();
 
   start.addEventListener('click', () => {
+    if(start.classList.contains('inactive')){
+      return;
+    }
     setButtonStateRunning();
     startTime = Date.now();
     countUp();
   });
 
   stop.addEventListener('click', () => {
+    if(stop.classList.contains('inactive')){
+      return;
+    }
+
     setButtonStateStopped();
     clearTimeout(timeoutId);
     elapsedTime += Date.now() - startTime;
   });
 
   reset.addEventListener('click', () => {
+    if(reset.classList.contains('inactive')){
+      return;
+    }
+
     setButtonStateInitial();
     timer.textContent = '00:00.000';
     elapsedTime = 0;
